06565f9443c6ab62509befb3670b8fdbddddad74
afirewillrise <monishkumar1154@gmail.com>
2016-08-24 16:06:16 -0400

    Add TodoList and Todo Static Components

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,11 +1,35 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function() {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: "Walk the talk"
+        },
+        {
+          id: 2,
+          text: 'Be thoughtful'
+        },
+        {
+          id: 3,
+          text: 'Finish This'
+        },
+        {
+          id: 4,
+          text: 'COMMONNN!'
+        }
+      ]
+    }
+  },
   render: function() {
+    var {todos} = this.state;
+
     return (
       <div>
-        <h1>Todo App</h1>
-        <p>This is the Todo App component.</p>
+        <TodoList todos={todos} />
       </div>
     )
   }

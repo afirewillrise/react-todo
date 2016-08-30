eb549226e55645bbaf0076ccd68dfcfa76b5047f
afirewillrise <monishkumar1154@gmail.com>
2016-08-30 09:00:17 -0400

    Add search and filter functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -58,4 +58,49 @@ describe('TodoAPI', () => {
       expect(actualTodos).toEqual(todos);
     });
   });
+
+  describe('filterTodos', () => {
+    var todos = [
+      {
+        id: 1,
+        text: 'First Test Todo',
+        completed: true
+      },
+      {
+        id: 2,
+        text: 'Second Test Todo',
+        completed: false
+      },
+      {
+        id: 3,
+        text: 'Third Test Todo',
+        completed: true
+      }
+    ];
+
+    it('should return all todos when showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return only not completed todos when showCompleted is false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should return all todos when searchText is empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return only matching todos when searchText is given', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'third');
+      expect(filteredTodos.length).toBe(1);
+    });
+  });
 });

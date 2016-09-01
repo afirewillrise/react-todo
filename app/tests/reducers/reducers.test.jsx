b38ef19ea803102129cf91b1f2539f7773ec90a8
afirewillrise <monishkumar1154@gmail.com>
2016-09-01 14:37:44 -0400

    Add todosreducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -28,4 +28,57 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todos', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo completed state', () => {
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+      };
+
+      var action2 = {
+        type: 'TOGGLE_TODO',
+        id: 2
+      };
+
+      var todos = [
+        {
+          id: 1,
+          text: "Walk the dog",
+          completed: false,
+          createdAt: 32843824,
+          completedAt: undefined
+        },
+        {
+          id: 2,
+          text: "Talk to the dog",
+          completed: true,
+          createdAt: 32843824,
+          completedAt: 32843825
+        }
+      ];
+
+      var res = reducers.todosReducer(df(todos),df(action));
+
+      expect(res[0].completed).toBe(true);
+      expect(res[0].completedAt).toBeA('number');
+
+      var res = reducers.todosReducer(df(todos),df(action2));
+
+      expect(res[1].completed).toBe(false);
+      expect(res[1].completedAt).toBeA('undefined');
+    });
+  });
 });

4e70ef1838e1e2553794bd04ebb4a34aa3eaf531
afirewillrise <monishkumar1154@gmail.com>
2016-09-02 07:04:24 -0400

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,17 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('New State ', store.getState());
+});
+
+store.dispatch(actions.addTodo('Clean shelf'));
+store.dispatch(actions.setSearchText('shelf'));
+store.dispatch(actions.toggleShowCompleted());
+
 //Loading foundation
 $(document).foundation();
 

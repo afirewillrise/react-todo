7e5d6a4ca4f7d806ca8237be340297815391773a
afirewillrise <monishkumar1154@gmail.com>
2016-09-14 05:39:36 -0400

    Move credentials to env files

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,10 +2,11 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyC239AewulT27xrTB20djgPV-YoqXaFOHM",
-    authDomain: "react-todo-demo-75fe6.firebaseapp.com",
-    databaseURL: "https://react-todo-demo-75fe6.firebaseio.com",
-    storageBucket: "react-todo-demo-75fe6.appspot.com",
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
   firebase.initializeApp(config);
 } catch (e) {
